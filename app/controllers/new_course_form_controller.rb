a5aa07ee5a842d474781e157f583cdb7363fa795
Ron Santos <ron.santos@gmail.com>
2013-02-12 14:05:12 -0800

    Added method to get user roles from Amaint and a check if student only

--- app/controllers/new_course_form_controller.rb
+++ app/controllers/new_course_form_controller.rb
@@ -1,8 +1,5 @@
-class Course
-  attr_accessible :course_sections_attributes
-  accepts_nested_attributes_for :course_sections
-
-end
+require "rest_client"
+require "json"
 
 class NewCourseFormController < ApplicationController
   unloadable
@@ -13,4 +10,20 @@ class NewCourseFormController < ApplicationController
     @terms = Account.find_by_name('Simon Fraser University').enrollment_terms.delete_if {|t| t.name == 'Default Term'}
     10.times { @course.course_sections.build }
   end
+
+  def get_user_roles_from_amaint (sfuid)
+    auth_token = "not saved on github"
+    rest_url = "https://rest.its.sfu.ca/cgi-bin/WebObjects/AOBRestServer.woa/rest/datastore2/global/accountInfo.js?art=" + auth_token + "&username=" + sfuid
+    json_out = RestClient.get rest_url
+    account = JSON.parse json_out
+    account['roles']
+  end
+
+  def student_only? (sfuid)
+    if get_user_roles_from_amaint(sfuid).equal? "undergrad"
+      return false
+    end
+    return true
+  end
+
 end
